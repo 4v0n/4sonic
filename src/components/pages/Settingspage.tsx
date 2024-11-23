6eb2f41b6f308c10a0701958d0fb24f00b45da5b
4v0n <navodhk@hotmail.com>
2024-11-23 16:29:55 +0000

    Add setting reset button

--- src/components/pages/Settingspage.tsx
+++ src/components/pages/Settingspage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Toggle from "../shared/Toggle";
+import Button from "../shared/Button";
 
 export function Settingspage() {
 
@@ -7,26 +8,40 @@ export function Settingspage() {
 
   const [darkMode, setDarkMode] = useState(currentState === "enabled");
 
-  const enableDark = () => {
+  function enableDark() {
     setDarkMode(true);
 
     localStorage.setItem("dark-mode", "enabled");
     document.documentElement.classList.add("dark");
   };
 
-  const disableDark = () => {
+  function disableDark() {
     setDarkMode(false);
 
     localStorage.setItem("dark-mode", "disabled");
     document.documentElement.classList.remove("dark");
   };
 
+  function resetSettings() {
+    localStorage.clear();
+    window.location.reload();
+  };
+
   return (
     <div className="flex justify-center mt-4">
-      <div className="w-full mac-w-md px-4">
+      <div className="w-full max-w-md px-4 ">
+
         <div className="flex justify-between items-center ml-16 mr-16">
           <h1 className="font-medium">Dark Mode</h1>
           <Toggle initialState={darkMode} onEnable={disableDark} onDisable={enableDark} />
+        </div>
+
+        <div className="flex justify-center mt-6">
+          <Button
+            onClick={resetSettings}
+          >
+            Reset Application
+          </Button>
         </div>
       </div>
     </div>
