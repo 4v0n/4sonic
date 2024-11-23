6a8cda4832b78cd9921c4f82a2f5839726b231c9
4v0n <navodhk@hotmail.com>
2024-11-23 01:25:02 +0000

    Extend ClickableIcon into button component

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,28 +1,45 @@
 import React from "react";
 
 interface ClickableIconProps {
-  icon: string;
+  icon?: string;
   onClick?: () => void;
   onMouseEnter?: () => void;
   onMouseLeave?: () => void;
+  children?: React.ReactNode;
 }
 
-const Button: React.FC<ClickableIconProps> = ({
+export default function({
   icon,
   onClick,
   onMouseEnter,
   onMouseLeave,
-}) => {
+  children,
+}: ClickableIconProps) {
+
+  if (children == undefined) {
+    return (
+      <button
+        className="button flex items-center justify-center min-h-8 min-w-8 rounded-full cursor-pointer"
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        {icon && (<img src={icon} alt="icon" className="object-contain" />)}
+      </button>
+    );
+  }
+
   return (
     <button
-      className="button flex items-center justify-center w-8 h-8 rounded-full cursor-pointer"
+      className="button flex items-center min-h-8 min-w-8 px-2 rounded-full cursor-pointer"
       onClick={onClick}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
     >
-      <img src={icon} alt="icon" className="object-contain" />
+      <div className="flex items-center justify-start">
+        {icon && (<img src={icon} alt="icon" className="object-contain" />)}
+        {children}
+      </div>
     </button>
   );
 };
-
-export default Button;
